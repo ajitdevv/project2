65cca474cd2ea67a424fb5419116f492c645081e
ajitdevv <ajitdev014@gmail.com>
2025-09-09 19:20:30 +0530

    add some future

--- script.js
+++ script.js
@@ -1,7 +1,93 @@
-console.log("hy i am prince")
-var a=2;
-var b=3;
-console.log(
-    a
-    +b+4
-)
+// let a = 5;
+// if (a < 5) {
+//     console.log("a is smaller than 5");
+// }
+// else if (a == 5) {
+//     console.log("a is equal to 5");
+// }
+// else {
+//     console.log("a is greater than 5");
+// }
+// a = 10;
+// b = 20;
+
+// let c = a > b ? a + b : b - a;
+// console.log(c);
+// let a = '4';
+// let b = a + 3;
+// console.log(b);
+// c = typeof (b);
+// console.log(c);
+// let age = 13
+// if (age <=10) {
+
+//     console.log("you are not elegable")
+// }
+
+// else if (age >=10 && age <=19){
+
+//     console.log("you are elegable")
+// }
+// else{
+//     console.log("you are not to not elegable")
+// }
+
+
+// ?prectis 2===4
+// let a = 10;
+// if (a / 3 || a / 2) {
+//     console.log("this his best for me")
+
+// }
+// else {
+//     console.log("this his not good for me ")
+// }
+
+
+// ? ternary operater in java scripet
+// let age = 18;
+// let a = age <= 18 ? console.log("you are not elageble") : console.log("your are elageble")
+
+
+
+
+// ?loops 
+// a=1
+// for (let i = 0; i < 6; i++) {
+//     console.log(a+i)
+
+// }
+
+// while(a<6){
+//     console.log(a)
+//     a++;
+// }
+
+
+
+// ?functions
+function namefuc(name) {
+    console.log("hey " + name + " i am good")
+    console.log("hey " + name + " i to am good")
+    console.log("hey " + name + " my name his" + name)
+    console.log("hey " + name + " i a good explorar")
+
+
+
+}
+
+namefuc("raju")
+namefuc(2)
+function sum(a,b) {
+    console.log(a+b)
+    
+}
+sum(2,4)
+
+const Func=(w)=>{
+    console.log("hy this his a arreo function",w)
+
+}
+Func(34)
+Func(74)
+Func(37)
