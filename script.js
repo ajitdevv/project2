f14ca07938f148166bd53bdfe583b70e693d6a8f
ajitdevv <ajitdev014@gmail.com>
2025-09-10 07:53:24 +0530

    add some future

--- script.js
+++ script.js
@@ -105,14 +105,14 @@
 // }
 
 // find the sum of 1 to 100 using a loop
-// let sum=0
-// for (let i = 1; i <= 100; i++) {
-//      sum=sum+i
+let sum=0
+for (let i = 1; i <= 100; i++) {
+     sum=sum+i
 
 
 
-// }
-// console.log(sum)
+}
+console.log(sum)
 
 // print numbers this numbers devided by 3 and 5 both in 1 to 50
 
