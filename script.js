f98e3e59f45c42cff1f3948ca730f87a65388940
ajitdevv <ajitdev014@gmail.com>
2025-09-10 07:57:02 +0530

    add some future

--- script.js
+++ script.js
@@ -90,13 +90,13 @@
 //     }
 
 // print odd numbers from 1 to 15 using while loop
-// let i =1
-// while (i<=15) {
-//     if (i % 2 === 1) {
-//         console.log(i)
-//     }
-//     i++
-// }
+let i = 1;
+while (i <= 15) {
+    if (i % 2 === 1) {
+        console.log(i)
+    }
+    i++
+}
 
 // print a 5 table
 // for (let i = 1; i <=10; i++) {
@@ -105,104 +105,104 @@
 // }
 
 // find the sum of 1 to 100 using a loop
-let sum=0
-for (let i = 1; i <= 100; i++) {
-     sum=sum+i
+// let sum=0
+// for (let i = 1; i <= 100; i++) {
+//      sum=sum+i
 
 
 
-}
-console.log(sum)
+// }
+// console.log(sum)
 
 // print numbers this numbers devided by 3 and 5 both in 1 to 50
 
-for (let i = 0; i <= 50; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log(i)
+// for (let i = 0; i <= 50; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log(i)
 
-    }
+//     }
 
 
-}
+// }
 
 // take a number by user and program explan ever number his even or odd
-let num = prompt(`type any number`)
-for (let i = 1; i < num; i++) {
-    if (i % 2 === 0) {
-        console.log(`${i}: his a even`)
+// let num = prompt(`type any number`)
+// for (let i = 1; i < num; i++) {
+//     if (i % 2 === 0) {
+//         console.log(`${i}: his a even`)
 
-    } else {
-        console.log(`${i}:his odd`)
-    }
-}
+//     } else {
+//         console.log(`${i}:his odd`)
+//     }
+// }
 
 
 
 
 // ?functions
-function namefuc(name) {
-    console.log("hey " + name + " i am good")
-    console.log("hey " + name + " i to am good")
-    console.log("hey " + name + " my name his" + name)
-    console.log("hey " + name + " i a good explorar")
+// function namefuc(name) {
+//     console.log("hey " + name + " i am good")
+//     console.log("hey " + name + " i to am good")
+//     console.log("hey " + name + " my name his" + name)
+//     console.log("hey " + name + " i a good explorar")
 
 
 
-}
+// }
 
-function abcd(...raju) {
-    console.log(raju);
-}
-abcd(1,2,3,4,5,6,7,8)
+// function abcd(...raju) {
+//     console.log(raju);
+// }
+// abcd(1,2,3,4,5,6,7,8)
 
-let valuse ={
-name:"prince",
-age: 19,
-gmail:"fghjk@gmail.com",
-phone:"+91-1234567890"
+// let valuse ={
+// name:"prince",
+// age: 19,
+// gmail:"fghjk@gmail.com",
+// phone:"+91-1234567890"
 
-}
-for (let raj in valuse) {
-    console.log(raj,valuse[raj])
-}
-namefuc("raju")
-namefuc(2)
-function sum(a,b) {
-    console.log(a+b)
+// }
+// for (let raj in valuse) {
+//     console.log(raj,valuse[raj])
+// }
+// namefuc("raju")
+// namefuc(2)
+// function sum(a,b) {
+//     console.log(a+b)
 
-}
-sum(2,4)
+// }
+// sum(2,4)
 
-const Func=(w)=>{
-    console.log("hy this his a arreo function",w)
+// const Func=(w)=>{
+//     console.log("hy this his a arreo function",w)
 
-}
-Func(34)
-Func(74)
-Func(37)
-
-
-/*  creat a faulty calculater using java script
-this faulty calculater peroform dose following:
-1. its his tack two numbers by user inputes.
-2.this his peroform operations ilke this:
-+ ---> -
-- ---> /
-* ---> +
-/ ---> **
-its his do this 10% times
-*/
+// }
+// Func(34)
+// Func(74)
+// Func(37)
 
 
-function rps(usre, computer) {
-    if (usre === computer) return "match his drow";
-    if (usre === "roak" && computer === "siger") return "usre win";
-    if (usre === "siger" && computer === "paper") return "user win";
-    if (usre === "paper" && computer === "roak") return "user win";
-    if (usre === "siger" && computer === "roak") return "computer win";
-    if (usre === "paper" && computer === "siger") return "computer win";
-    if (usre === "roak" && computer === "paper") return "computer win";
+//  creat a faulty calculater using java script
+// this faulty calculater peroform dose following:
+// 1. its his tack two numbers by user inputes.
+// 2.this his peroform operations ilke this:
+// + ---> -
+// - ---> /
+// * ---> +
+// / ---> **
+// its his do this 10% times
+// 
 
 
-}
-console.log (rps("paper", "siger"));
+// function rps(usre, computer) {
+//     if (usre === computer) return "match his drow";
+//     if (usre === "roak" && computer === "siger") return "usre win";
+//     if (usre === "siger" && computer === "paper") return "user win";
+//     if (usre === "paper" && computer === "roak") return "user win";
+//     if (usre === "siger" && computer === "roak") return "computer win";
+//     if (usre === "paper" && computer === "siger") return "computer win";
+//     if (usre === "roak" && computer === "paper") return "computer win";
+
+
+// }
+// console.log (rps("paper", "siger"));
