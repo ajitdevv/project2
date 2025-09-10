a29c5eb8668e09dd21153d717120ac109ff516af
ajitdevv <ajitdev014@gmail.com>
2025-09-10 08:01:05 +0530

    add some future

--- script.js
+++ script.js
@@ -1,108 +1,108 @@
-let a = 5;
-if (a < 5) {
-    console.log("a is smaller than 5");
-}
-else if (a == 5) {
-    console.log("a is equal to 5");
-}
-else {
-    console.log("a is greater than 5");
-}
-a = 10;
-b = 20;
+// let a = 5;
+// if (a < 5) {
+//     console.log("a is smaller than 5");
+// }
+// else if (a == 5) {
+//     console.log("a is equal to 5");
+// }
+// else {
+//     console.log("a is greater than 5");
+// }
+// a = 10;
+// b = 20;
 
-let c = a > b ? a + b : b - a;
-console.log(c);
-let a = '4';
-let b = a + 3;
-console.log(b);
-c = typeof (b);
-console.log(c);
-let age = 13
-if (age <=10) {
+// let c = a > b ? a + b : b - a;
+// console.log(c);
+// let a = '4';
+// let b = a + 3;
+// console.log(b);
+// c = typeof (b);
+// console.log(c);
+// let age = 13
+// if (age <=10) {
 
-    console.log("you are not elegable")
-}
+//     console.log("you are not elegable")
+// }
 
-else if (age >=10 && age <=19){
+// else if (age >=10 && age <=19){
 
-    console.log("you are elegable")
-}
-else{
-    console.log("you are not to not elegable")
-}
-
-
-?prectis 2===4
-let a = 10;
-if (a / 3 || a / 2) {
-    console.log("this his best for me")
-
-}
-else {
-    console.log("this his not good for me ")
-}
+//     console.log("you are elegable")
+// }
+// else{
+//     console.log("you are not to not elegable")
+// }
 
 
-? ternary operater in java scripet
-let age = 18;
-let a = age <= 18 ? console.log("you are not elageble") : console.log("your are elageble")
+// ?prectis 2===4
+// let a = 10;
+// if (a / 3 || a / 2) {
+//     console.log("this his best for me")
+
+// }
+// else {
+//     console.log("this his not good for me ")
+// }
+
+
+// ? ternary operater in java scripet
+// let age = 18;
+// let a = age <= 18 ? console.log("you are not elageble") : console.log("your are elageble")
 
 
 
 
-?loops 
-a=1
-for (let i = 0; i < 6; i++) {
-    console.log(a+i)
+// ?loops 
+// a=1
+// for (let i = 0; i < 6; i++) {
+//     console.log(a+i)
 
-}
+// }
 
-while(a<6){
-    console.log(a)
-    a++;
-}
+// while(a<6){
+//     console.log(a)
+//     a++;
+// }
 
-let i=1;
-while (i<=35) {
-   console.log(i) 
-    i++
+// let i=1;
+// while (i<=35) {
+//    console.log(i) 
+//     i++
 
-}
+// }
 //  1 to 10 using for loop
 
-for (let i=1; i <= 10; i--) {
-    console.log(i)
+// for (let i=1; i <= 10; i--) {
+//     console.log(i)
 
-}
+// }
 // 20 to 1 using while loop 
-let raj = 20
-while (raj>=1) {
-    console.log(raj)
-    raj--
+// let raj = 20
+// while (raj>=1) {
+//     console.log(raj)
+//     raj--
 
-}
+// }
 
 // print even numbers from 1 to 20
-for (let i = 1; i <= 20; i++) {
-    if (i % 2 === 0) {
-        console.log(i)
-    }
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
 
 // print odd numbers from 1 to 15 using while loop
-let i = 1;
-while (i <= 15) {
-    if (i % 2 === 1) {
-        console.log(i)
-    }
-    i++
-}
+// let i = 1;
+// while (i <= 15) {
+//     if (i % 2 === 1) {
+//         console.log(i)
+//     }
+//     i++
+// }
 
 // print a 5 table
-for (let i = 1; i <=10; i++) {
-    console.log(`5 * ${i} = ${5*i}`)
+// for (let i = 1; i <=10; i++) {
+//     console.log(`5 * ${i} = ${5*i}`)
 
-}
+// }
 
 // find the sum of 1 to 100 using a loop
 // let sum=0
