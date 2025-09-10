a8f3cb5d7b17d4e5632c6fb2f58f7d8c1ab25c5e
ajitdevv <ajitdev014@gmail.com>
2025-09-10 07:59:46 +0530

    add some future

--- script.js
+++ script.js
@@ -1,74 +1,74 @@
-// let a = 5;
-// if (a < 5) {
-//     console.log("a is smaller than 5");
-// }
-// else if (a == 5) {
-//     console.log("a is equal to 5");
-// }
-// else {
-//     console.log("a is greater than 5");
-// }
-// a = 10;
-// b = 20;
+let a = 5;
+if (a < 5) {
+    console.log("a is smaller than 5");
+}
+else if (a == 5) {
+    console.log("a is equal to 5");
+}
+else {
+    console.log("a is greater than 5");
+}
+a = 10;
+b = 20;
 
-// let c = a > b ? a + b : b - a;
-// console.log(c);
-// let a = '4';
-// let b = a + 3;
-// console.log(b);
-// c = typeof (b);
-// console.log(c);
-// let age = 13
-// if (age <=10) {
+let c = a > b ? a + b : b - a;
+console.log(c);
+let a = '4';
+let b = a + 3;
+console.log(b);
+c = typeof (b);
+console.log(c);
+let age = 13
+if (age <=10) {
 
-//     console.log("you are not elegable")
-// }
+    console.log("you are not elegable")
+}
 
-// else if (age >=10 && age <=19){
+else if (age >=10 && age <=19){
 
-//     console.log("you are elegable")
-// }
-// else{
-//     console.log("you are not to not elegable")
-// }
-
-
-// ?prectis 2===4
-// let a = 10;
-// if (a / 3 || a / 2) {
-//     console.log("this his best for me")
-
-// }
-// else {
-//     console.log("this his not good for me ")
-// }
+    console.log("you are elegable")
+}
+else{
+    console.log("you are not to not elegable")
+}
 
 
-// ? ternary operater in java scripet
-// let age = 18;
-// let a = age <= 18 ? console.log("you are not elageble") : console.log("your are elageble")
+?prectis 2===4
+let a = 10;
+if (a / 3 || a / 2) {
+    console.log("this his best for me")
+
+}
+else {
+    console.log("this his not good for me ")
+}
+
+
+? ternary operater in java scripet
+let age = 18;
+let a = age <= 18 ? console.log("you are not elageble") : console.log("your are elageble")
 
 
 
 
-// ?loops 
-// a=1
-// for (let i = 0; i < 6; i++) {
-//     console.log(a+i)
+?loops 
+a=1
+for (let i = 0; i < 6; i++) {
+    console.log(a+i)
 
-// }
+}
 
-// while(a<6){
-//     console.log(a)
-//     a++;
-// }
+while(a<6){
+    console.log(a)
+    a++;
+}
 
-// let i=1;
-// while (i<=35) {
-//    console.log(i) 
-//     i++
+let i=1;
+while (i<=35) {
+   console.log(i) 
+    i++
 
-// }
+}
 //  1 to 10 using for loop
 
 for (let i=1; i <= 10; i--) {
